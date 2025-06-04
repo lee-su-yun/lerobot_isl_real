89790f2eb0b3ff14a4bd6b385d9448e524919bd2
isuyun <sylee@islab.snu.ac.kr>
2025-06-04 17:12:51 +0900

    training

--- lerobot/configs/policies.py
+++ lerobot/configs/policies.py
@@ -66,7 +66,8 @@ class PreTrainedConfig(draccus.ChoiceRegistry, HubMixin, HFPretrainedConfig, abc
         if not self.device or not is_torch_device_available(self.device):
             auto_device = auto_select_torch_device()
             logging.warning(f"Device '{self.device}' is not available. Switching to '{auto_device}'.")
-            self.device = auto_device.type
+            #self.device = auto_device.type
+            self.device = "cuda:5"
 
         # Automatically deactivate AMP if necessary
         if self.use_amp and not is_amp_available(self.device):
