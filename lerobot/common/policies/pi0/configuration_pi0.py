3795bd7f7869e19381c820e7991a60387e91a94f
isuyun <sylee@islab.snu.ac.kr>
2025-06-06 13:46:38 +0900

    training (action_chunk = 5)

--- lerobot/common/policies/pi0/configuration_pi0.py
+++ lerobot/common/policies/pi0/configuration_pi0.py
@@ -27,8 +27,8 @@ from lerobot.configs.types import FeatureType, NormalizationMode, PolicyFeature
 class PI0Config(PreTrainedConfig):
     # Input / output structure.
     n_obs_steps: int = 1
-    chunk_size: int = 50
-    n_action_steps: int = 50
+    chunk_size: int = 5
+    n_action_steps: int = 5
 
     normalization_mapping: dict[str, NormalizationMode] = field(
         default_factory=lambda: {
