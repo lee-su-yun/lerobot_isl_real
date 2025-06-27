1a2be3ff6d19104d75fa664b5bd7d11a113cd74d
isuyun <sylee@islab.snu.ac.kr>
2025-06-27 14:44:32 +0900

    run ddp pi0

--- custom_scripts/scripts/run.sh
+++ custom_scripts/scripts/run.sh
@@ -1,17 +1,17 @@
-DEVICES=0,1,2,3,4,5
+DEVICES=0,1,2,3
 TORCH_DISTRIBUTED_BUCKET_CAP_MB=10
 
 CUDA_VISIBLE_DEVICES=${DEVICES} \
   torchrun \
-    --nproc_per_node=6 \
+    --nproc_per_node=4 \
     ./train_pi0_ddp.py \
     --policy.path=/ckpt/pi0 \
     --use_ddp=true \
-    --dataset.repo_id=/data/piper_subtask_data/pick/train_5hz_real \
-    --dataset.root=/data/piper_subtask_data/pick/train_5hz_real \
+    --dataset.repo_id=/data/piper_grape0626/lerobot_5hz \
+    --dataset.root=/data/piper_grape0626/lerobot_5hz \
     --wandb.enable=true \
-    --output_dir=/result/piper_subtask/pi0_test \
-    --job_name=pi0_ddp_piper_pick \
+    --output_dir=/result/piper_grape0627 \
+    --job_name=pi0_ddp_piper_grape \
     --wandb.disable_artifact=true \
     --batch_size=6 \
     --num_workers=16 \
