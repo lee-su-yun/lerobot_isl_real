5e86d6dc7edbcb880337f8a1b782da25659b53e4
isuyun <sylee@islab.snu.ac.kr>
2025-07-07 15:37:08 +0900

    MoE vision encoder (data_setting)

--- custom_scripts/scripts/data_set.py
+++ custom_scripts/scripts/data_set.py
@@ -3,17 +3,17 @@ import pandas as pd
 from pathlib import Path
 import shutil
 
-for j in range(12):
-    data_dir = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/data/chunk-{j:03d}")
-    save_data_dir = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/data/chunk-{j+12:03d}")
-    for i in range(50):
-        filename = f"episode_{i+50*j:06d}.parquet"
-        file_path = data_dir / filename
-        # Load
-        df = pd.read_parquet(file_path)
-        # Modify columns
-        new_filename = f"episode_{i + 50 * (j + 12):06d}.parquet"
-        df.to_parquet(save_data_dir / new_filename)
+# for j in range(12):
+#     data_dir = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/data/chunk-{j:03d}")
+#     save_data_dir = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/data/chunk-{j+12:03d}")
+#     for i in range(50):
+#         filename = f"episode_{i+50*j:06d}.parquet"
+#         file_path = data_dir / filename
+#         # Load
+#         df = pd.read_parquet(file_path)
+#         # Modify columns
+#         new_filename = f"episode_{i + 50 * (j + 12):06d}.parquet"
+#         df.to_parquet(save_data_dir / new_filename)
 
 
 for j in range(12):
