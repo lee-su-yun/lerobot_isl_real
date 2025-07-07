b377191a26cd694ffad6d29d78d307b1827a4cb9
isuyun <sylee@islab.snu.ac.kr>
2025-07-07 15:36:47 +0900

    MoE vision encoder (data_setting)

--- custom_scripts/scripts/data_set.py
+++ custom_scripts/scripts/data_set.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from pathlib import Path
+import shutil
 
 for j in range(12):
     data_dir = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/data/chunk-{j:03d}")
@@ -14,3 +15,29 @@ for j in range(12):
         new_filename = f"episode_{i + 50 * (j + 12):06d}.parquet"
         df.to_parquet(save_data_dir / new_filename)
 
+
+for j in range(12):
+    data_dir_exo = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j:03d}/observation.images.exo")
+    data_dir_table = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j:03d}/observation.images.table")
+    data_dir_wrist = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j:03d}/observation.images.wrist")
+
+    save_data_dir_exo = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j+12:03d}/observation.images.exo")
+    save_data_dir_table = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j+12:03d}/observation.images.table")
+    save_data_dir_wrist = Path(f"/data/piper_grape0626_multiview/lerobot_5hz/video/chunk-{j+12:03d}/observation.images.wrist")
+    for i in range(50):
+        global_index = i + 50 * j
+        new_index = i + 50 * (j + 12)
+        for cam, src_dir, dst_dir in zip(
+                ["exo", "table", "wrist"],
+                [data_dir_exo, data_dir_table, data_dir_wrist],
+                [save_data_dir_exo, save_data_dir_table, save_data_dir_wrist]
+        ):
+            filename = f"episode_{global_index:06d}.mp4"
+            new_filename = f"episode_{new_index:06d}.mp4"
+            src_file = src_dir / filename
+            dst_file = dst_dir / new_filename
+
+            if src_file.exists():
+                shutil.copy2(src_file, dst_file)
+            else:
+                print(f"Warning: {src_file} not found.")
