78209114e0e90e8e29ddd6dd3b7aa93c51e64c17
isuyun <sylee@islab.snu.ac.kr>
2025-07-07 16:31:09 +0900

    MoE vision encoder train

---
+++ custom_scripts/scripts/train_ours_pi0.sh
@@ -0,0 +1,22 @@
+DEVICES=0,1,2,3
+TORCH_DISTRIBUTED_BUCKET_CAP_MB=10
+
+CUDA_VISIBLE_DEVICES=${DEVICES} \
+  torchrun \
+    --master-port 29400 \
+    --nproc_per_node=4 \
+    ./train_pi0_ddp.py \
+    --policy.path=/ckpt/pi0 \
+    --use_ddp=true \
+    --dataset.repo_id=/data/piper_grape0626_multiview/lerobot_5hz \
+    --dataset.root=/data/piper_grape0626_multiview/lerobot_5hz \
+    --dataset.episodes=[]
+    --wandb.enable=true \
+    --output_dir=/result/pi0_ddp_20250707_piper_pickgrape_moe_test \
+    --job_name=pi0_ddp_moe_piper_pickgrape \
+    --wandb.disable_artifact=true \
+    --batch_size=6 \
+    --num_workers=16 \
+    --log_freq=10 \
+    --save_freq=10000 \
+    --steps=40000
